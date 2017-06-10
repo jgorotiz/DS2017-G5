9be315e186c8c0876873504947e47a5cad044b86
José Luis Massón <jlmasson@espol.edu.ec>
2017-06-10 10:24:10 -0500

    Clase Usuario

--- SmartFood/src/smartfood/classes/user/Usuario.java
+++ SmartFood/src/smartfood/classes/user/Usuario.java
@@ -5,10 +5,108 @@
  */
 package smartfood.classes.user;
 
+import java.sql.Date;
+
 /**
  *
  * @author Jose Masson
  */
 public class Usuario {
     
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String usuario;
+    private String contrasenia;
+    private String rol;
+    private Date fechaRegistro;
+    private boolean activo;
+
+    public Usuario(int idUsuario, String nombre, String apellido, String correo,
+            String usuario, String contrasenia, String rol, Date fechaRegistro, 
+            boolean activo) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.rol = rol;
+        this.fechaRegistro = fechaRegistro;
+        this.activo = activo;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+            
 }
