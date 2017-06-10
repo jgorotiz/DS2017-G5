0094f46c9b8ec01645490741848d690d9a3ae53e
Jonathan <jgorotiz@espol.edu.ec>
2017-06-10 17:56:22 -0500

    consulta y actualizacion de plato

--- sql/procedimientos.sql
+++ sql/procedimientos.sql
@@ -10,26 +10,61 @@ begin
 end//
 
 delimiter ;
-
 /* platillos por restaurante dada una categoria*/
 delimiter //
 create procedure platilloXCategoria(in categoria varchar(50))
 begin
 	select nombre, descripcion, img, nombreTipo, nombreCategoria
-    from plato,categoria
-    where plato.categoria =  categoria.idCategoria and categoria = nombreCategoria ;
+    from plato,categoria, tipo
+    where plato.categoria =  categoria.idCategoria and plato.tipo =  tipo.idTipo and  categoria = nombreCategoria ;
 end// 
 
 delimiter ;
-    
+
+
 /* platillos por restaurante dado un tipo*/
 delimiter //
-create procedure platilloXTipo(in categoria varchar(50))
+create procedure platilloXTipo(in tipo varchar(50))
 begin
 	select nombre, descripcion, img, nombreTipo, nombreCategoria
-    from plato,categoria
-    where plato.categoria =  categoria.idCategoria;
+    from plato,tipo, categoria
+    where plato.tipo =  tipo.idTipo and plato.categoria =  categoria.idCategoria and tipo = tipo.nombreTipo;
 end// 
-
 delimiter ;
 
+/* platillos dada una descripcion*/
+delimiter //
+create procedure platilloXDescripcion(in nDescripcion varchar(70))
+begin
+	select nombre, descripcion, img, nombreTipo, nombreCategoria
+    from plato,tipo, cateogria
+    where plato.tipo =  tipo.idTipo and plato.categoria =  categoria.idCategoria and descripcion like nDescripcion;
+end// 
+delimiter ;
+
+/* platillos dado un nombre*/
+delimiter //
+create procedure platilloXNombre(in nNombre varchar(70))
+begin
+	select nombre, descripcion, img, nombreTipo, nombreCategoria
+    from plato,tipo, cateogria
+    where plato.tipo =  tipo.idTipo and plato.categoria =  categoria.idCategoria and nombre like nNombre;
+end// 
+delimiter ;
+
+/* actualizar valores de platillos*/
+delimiter //
+create procedure actualizarPlatilo(in nIdPlato int, in nuevoNombre varchar(50),in nuevaDescripcion varchar(200), in nuevaImagen varchar(200),
+	in nuevaCategoria int, in nuevoTipo int, in nuevoRestaurante int, in nuevaFechaRegistro datetime)
+begin
+	update palto
+    set nombre = nuevoNombre,
+		decripcion = nuevaDescripcion,
+        imagen = nuevaImagen,
+        categoria = nuevaCategoria,
+        tipo  = nuevoTipo,
+        restaurante = nuevoRestaurante,
+        fechaRegistro = nuevaFechaRegistro
+	where idPlato = nIdPlato;
+end// 
+delimiter ;
