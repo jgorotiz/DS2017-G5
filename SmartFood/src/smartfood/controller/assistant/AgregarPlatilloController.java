651f0403776728d080973918eb56c1e73543fb86
José Luis Massón <jlmasson@espol.edu.ec>
2017-06-11 12:38:19 -0500

    Pantallde Agregar Platillo casi lista

---
+++ SmartFood/src/smartfood/controller/assistant/AgregarPlatilloController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartfood.controller.assistant;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Jose Masson
+ */
+public class AgregarPlatilloController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    
+    }
+    
+}
